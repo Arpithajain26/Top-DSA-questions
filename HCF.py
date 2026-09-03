e2bb2fa51e7e6ac744702c812433a1bb7267fbb5
arpitha <arpithaammujain39@gmail.com>
2026-09-03 19:17:37 +0530

    changed HCF to HCF_func

--- HCF.py
+++ HCF.py
@@ -1,7 +1,7 @@
-def HCF(a,b):
+def HCF_func(a,b):
     HCF=float('-inf')
     for i in range(1,a+1):
         if a%i==0 and b%i==0:
             HCF=i
     return HCF
-print(HCF(10,20))
+print(HCF_func(10,20))
