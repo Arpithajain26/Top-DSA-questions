af875501ffdc8e5ae50642580c369b95e14fa20e
arpitha <arpithaammujain39@gmail.com>
2026-09-15 14:42:21 +0530

    added new file

--- Atoi.py
+++ Atoi.py
@@ -35,7 +35,7 @@ Example 5:
 
 Input: "+123"
 Output: 123"""
-def atoi(s):
+"""def atoi(s):
     s=s.strip()
     list1=[]
     list2=['-']
@@ -49,9 +49,41 @@ def atoi(s):
     if list1==[] or list1=='+' or list1=='-':
         return 0
     return int("".join(list1))
-print(atoi("42"))                 # 42
-print(atoi("+42"))                # 42
-print(atoi("-123"))               # -123
-print(atoi("   456"))             # 456
-print(atoi("4193 with words"))    # 4193
-print(atoi("words 987"))  
+"""
+def atoi_number(s):
+    s=s.strip()
+    list1=[]
+    list2=['+','-']
+    for i in s:
+        if i.isdigit():
+            list1.append(i)
+        elif i in list2 and len(list1)==0:
+            list1.append(i)
+        else:
+            break
+    if list1==[] or list1==['+'] or list1==['-']:
+        return 0
+    sign=1
+        
+    if list1[0]=='-':
+        sign=-1
+        list1.pop(0)
+    elif list1[0]=='+':
+        sign=1
+        list1.pop(0)
+    num=0
+    for i in list1:
+        num=num*10+(ord(i)-ord('0'))
+    num=num*sign
+    if num> 2147483647:
+        return  2147483647
+    elif num <-2147483648:
+        return -2147483648
+    return num
+print(atoi_number("42"))                 # 42
+print(atoi_number("+42"))                # 42
+print(atoi_number("-123"))               # -123
+print(atoi_number("   456"))             # 456
+print(atoi_number("4193 with words"))    # 4193
+print(atoi_number("words 987"))  
+    
---
+++ binary_of_a_number.py
@@ -0,0 +1,51 @@
+"""🧩 Binary of a Number — Coding Question
+
+Question:
+
+Write a Python program to convert a given decimal number into its binary representation without using Python's built-in bin() function.
+
+Example 1:
+
+Input: 10
+Output: 1010
+
+Explanation:
+
+10 ÷ 2 → remainder 0
+5  ÷ 2 → remainder 1
+2  ÷ 2 → remainder 0
+1  ÷ 2 → remainder 1
+
+Reading remainders from bottom to top:
+1010
+
+Example 2:
+
+Input: 7
+Output: 111
+
+Example 3:
+
+Input: 15
+Output: 1111
+💡 Hint
+
+Use:
+
+% 2 → to get the remainder
+// 2 → to reduce the number
+Remember that the remainders need to be read in reverse order.
+
+Try writing the code yourself first, and send it to me. I'll check it without directly giving you the solution."""
+def binary_number(num):
+    return bin(num)[2:]
+print(binary_number(3))
+"""another technique"""
+def binary_number1(num):
+    result=""
+    while num>0:
+        rem=num%2
+        result=str(rem)+result
+        num=num//2
+    return result
+print(binary_number1(3))
