beec8b2e8f0864206a89d067129040e5509fb673
arpitha <arpithaammujain39@gmail.com>
2026-09-15 15:08:27 +0530

    added new file

--- check_power_of_2.py
+++ check_power_of_2.py
@@ -1,1 +1,29 @@
-a=10
+"""ower of 2 – Coding Question
+
+Question:
+
+Write a Python program to check whether a given positive integer is a power of 2 or not.
+
+A number is a power of 2 if it can be expressed as:
+
+2⁰, 2¹, 2², 2³, ...
+
+Examples:
+
+Input: 16
+Output: Power of 2
+
+Explanation:
+16 = 2⁴
+Input: 18
+Output: Not a Power of 2"""
+def check_power_of_2(num):
+    x=1
+    for i in range(num):
+        x=2**i
+        if x==num:
+            return "true"
+    return "false"
+print(check_power_of_2(4))
+print(check_power_of_2(7))
+        
