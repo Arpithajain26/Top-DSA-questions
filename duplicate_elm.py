7d9fa6208c1ceb7619b3bc9c158c1e6595e705ef
arpitha <arpithaammujain39@gmail.com>
2026-09-16 06:49:40 +0530

    added new file

---
+++ duplicate_elm.py
@@ -0,0 +1,35 @@
+"""🟢 Duplicate Elements — LeetCode Question
+
+Question:
+Given an integer array nums, determine whether any value appears at least twice in the array.
+
+Return True if there is at least one duplicate element; otherwise, return False.
+
+Example 1:
+
+Input:  nums = [1, 2, 3, 1]
+Output: True
+Explanation: 1 appears twice.
+
+Example 2:
+
+Input:  nums = [1, 2, 3, 4]
+Output: False
+Explanation: All elements are distinct.
+
+Example 3:
+
+Input:  nums = [1, 1, 1, 3, 3]
+Output: True
+
+Expected approach: Use a set to keep track of elements already seen.
+
+Time: O(n)"""
+def duplicate_element(nums):
+    list1=[]
+    for i in nums:
+        if i in list1:
+            return True
+        list1.append(i)
+    return False
+print(duplicate_element([1,1,1,2,3]))
