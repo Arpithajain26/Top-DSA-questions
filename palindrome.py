567d92e87c077f40aa14e7d7e851f54cd264c2de
arpitha <arpithaammujain39@gmail.com>
2026-09-18 08:02:30 +0530

    added new file

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,37 @@
-def palindrome(s):
+"""LeetCode #20 — Valid Parentheses
+
+Question:
+Given a string s containing only the characters (, ), {, }, [ and ], determine whether the input string is valid.
+
+A string is valid if:
+
+Every opening bracket has a corresponding closing bracket.
+Brackets close in the correct order.
+Each closing bracket matches the most recent unmatched opening bracket.
+
+Examples:
+
+Input:  s = "()"
+Output: True
+Input:  s = "()[]{}"
+Output: True
+Input:  s = "(]"
+Output: False
+Input:  s = "([{}])"
+Output: True
+Input:  s = "([)]"
+Output: False
+Your task"""
+def valid_paranthesis(s):
     stack=[]
-    for i in s:
-        if i in "[({":
-            stack.append(i)
+    for ch in s:
+        if ch in '[({':
+            stack.append(ch)
         else:
             if not stack:
                 return False
             top=stack.pop()
-            if (i==']' and top!='[') or (i==')' and top!='(') or (i=='}' and top!='{'):
+            if (ch==']' and top!='[') or (ch==')' and top!='(') or (ch=='}' and top!='{'):
                 return False
     return len(stack)==0
-print(palindrome("{[[]]}"))
+print(valid_paranthesis("()[]{}"))
