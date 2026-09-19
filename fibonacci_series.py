a61d85fe6c092383c56f73567a1541ee4e852365
arpitha <arpithaammujain39@gmail.com>
2026-09-19 15:15:32 +0530

    added new file

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -8,4 +8,13 @@ def fibonacci_series(num):
 def fibonacci(num):
     for i in range(num):
         print(fibonacci_series(i),end=" ")
-fibonacci(2)
+# fibonacci(2)
+def fibonacci_numbers(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci_numbers(n-1)+fibonacci_numbers(n-2)
+    
+print(fibonacci_numbers(3))
