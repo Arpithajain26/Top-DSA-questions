b3d4fc0cc31b933cc69d01c0160b0161a250d161
arpitha <arpithaammujain39@gmail.com>
2026-09-19 14:24:37 +0530

    Added new file

--- binary_search.py
+++ binary_search.py
@@ -43,3 +43,17 @@ def binary_search(nums,target):
             low=mid+1
     return -1
 print(binary_search( [1, 3, 5, 7, 9, 11],7))
+def binary_search1(nums,target):
+    low=0
+    high=len(nums)-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]>target:
+            high=mid-1
+        else:
+            low=mid+1
+    return -1
+print(binary_search1([1,3,5,7,9,11],7))
+        
