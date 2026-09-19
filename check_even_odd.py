d94ee1c9960c2996f82492a9d2fe8f941c3d3038
arpitha <arpithaammujain39@gmail.com>
2026-09-19 14:32:47 +0530

    added new file

--- check_even_odd.py
+++ check_even_odd.py
@@ -4,3 +4,9 @@ def even_odd(num):
     else:
         return "odd"
 print(even_odd(5))
+def check_even_odd(num):
+    if num&1==0:
+        return "even"
+    else:
+        return "odd"
+print(check_even_odd(6))
