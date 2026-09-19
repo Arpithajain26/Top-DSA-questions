8243fd99bc69d45329fc56c3207578f85aefb515
arpitha <arpithaammujain39@gmail.com>
2026-09-19 14:39:04 +0530

    added new file

--- common_factors.py
+++ common_factors.py
@@ -3,3 +3,8 @@ def common_factor(a,b):
         if a%i==0 and b%i==0:
             print(i)
 common_factor(10,20)
+def common_factors(a,b):
+    for i in range(1,a+1):
+        if a%i==0 and b%i==0:
+            print(i)
+common_factors(10,20)
