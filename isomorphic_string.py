a1401a107a5b568b8437c73126203ddbc40acccd
arpitha <arpithaammujain39@gmail.com>
2026-09-20 11:07:20 +0530

    added new file

--- isomorphic_string.py
+++ isomorphic_string.py
@@ -9,3 +9,17 @@ def isomorphic_string(s,t):
             h[s[i]]=t[i]
     return True
 print(isomorphic_string("add","egg"))
+
+
+def isomorphic_string1(s,t):
+    h={}
+    for i in range(len(s)):
+        if s[i] in h and h[s[i]] not in t[i]:
+            return False
+        elif s[i] not in h and t[i] in h.values():
+            return False
+        else:
+            h[s[i]]=t[i]
+    return True
+    
+print(isomorphic_string1("add","egg"))
