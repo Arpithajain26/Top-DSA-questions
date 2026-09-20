9ab96bdfc62fb87d13e896b375e4fe0966f540de
arpitha <arpithaammujain39@gmail.com>
2026-09-20 11:10:45 +0530

    added new file

--- LCM.py
+++ LCM.py
@@ -6,3 +6,10 @@ def LCM(a,b):
     lcm=(a*b)//hcf
     return lcm
 print(LCM(10,20))
+def LCM(a,b):
+    hcf=float('-inf')
+    for i in range(1,a+1):
+        if a%i==0:
+            hcf=i
+    return (a*b)//hcf
+print(LCM(10,20))
