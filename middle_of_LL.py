ce2410455ce724690eb755aa12e363890f57aef3
arpitha <arpithaammujain39@gmail.com>
2026-09-20 11:39:57 +0530

    added new file

--- middle_of_LL.py
+++ middle_of_LL.py
@@ -26,8 +26,8 @@ class Solution:
         fast=head
         while fast and fast.next:
             slow=slow.next 
-            fast=fast.next.next
-        return slow 
+            fast=fast.next.next 
+        return slow
 head = ListNode(1)
 head.next = ListNode(2)
 head.next.next = ListNode(3)
