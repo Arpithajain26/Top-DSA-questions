571b1e60f8d3d1aaff822c6af468a4aec03d725a
arpitha <arpithaammujain39@gmail.com>
2026-09-20 12:00:08 +0530

    added new file

--- prime_number.py
+++ prime_number.py
@@ -31,3 +31,11 @@ def check_prime(n):
             return False
     return True
 print(check_prime(8))
+def check_prime_number(n):
+    if n<2:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+print(check_prime_number(9))
