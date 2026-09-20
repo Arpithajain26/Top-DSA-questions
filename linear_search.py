f07799cc8a19f7998372670417fc1cb6d8f6c2d9
arpitha <arpithaammujain39@gmail.com>
2026-09-20 11:25:33 +0530

    add new file

--- linear_search.py
+++ linear_search.py
@@ -4,3 +4,9 @@ def linear_search(nums,k):
             return i
     return -1
 print(linear_search([1,2,3,4,5,6],3))
+def linear_search(nums,target):
+    for i in nums:
+        if target==i:
+            return "found"
+    return "not found"
+print(linear_search([1,2,3,4,5,6,7],4))
