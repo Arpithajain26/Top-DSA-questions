1ac552175b60e52369f26e63fa0e23b613c3530f
arpitha <arpithaammujain39@gmail.com>
2026-09-20 12:08:30 +0530

    added new file

--- reverse.py
+++ reverse.py
@@ -1,3 +1,6 @@
 def reverse_string(s):
     return " ".join(s.split()[::-1])
 print(reverse_string("my name is arpitha"))
+def reverse_string1(s):
+    return " ".join(s.split()[::-1])
+print(reverse_string1("my name is arpithaa"))
