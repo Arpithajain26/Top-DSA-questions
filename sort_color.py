9ad648d3186cb6fbc610c6f0da767907083ec0c3
arpitha <arpithaammujain39@gmail.com>
2026-09-20 12:20:38 +0530

    Added new file

--- sort_color.py
+++ sort_color.py
@@ -38,5 +38,20 @@ def sort_colors(nums):
             high-=1
     return nums
 print(sort_colors([2,0,2,1,1,0]))
+def sort_colors1(nums):
+    low=0
+    high=len(nums)-1
+    mid=0
+    while mid<=high:
+        if nums[mid]==0:
+            nums[mid],nums[low]=nums[low],nums[mid]
+            mid+=1
+            low+=1
+        elif nums[mid]==1:
+            mid+=1
+        else:
+            nums[mid],nums[high]=nums[high],nums[mid]
+            high-=1
+    return nums
 
-    
+print(sort_colors1([2,0,2,1,1,0]))
