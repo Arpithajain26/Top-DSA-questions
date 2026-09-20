0e9c09678ff5a6c42484040ad37bb7a14a0bc0cb
arpitha <arpithaammujain39@gmail.com>
2026-09-20 11:45:24 +0530

    added new file

--- palindrome.py
+++ palindrome.py
@@ -35,3 +35,16 @@ def valid_paranthesis(s):
                 return False
     return len(stack)==0
 print(valid_paranthesis("()[]{}"))
+def check_palindrome(s):
+    stack=[]
+    for ch in s:
+        if ch in "[({":
+            stack.append(ch)
+        else:
+            if not stack:
+                return False
+            top=stack.pop()
+            if (top=='[' and ch!=']') or (top=='{' and ch!='}') or (top=='(' and ch!=')'):
+                return False
+    return len(stack)==0
+print(check_palindrome("()[]{}"))
